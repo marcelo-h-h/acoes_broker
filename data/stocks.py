faa27610f8da134b4f7796426bf4f15922c25439
Julia Cinel <49698954+jcinel@users.noreply.github.com>
2019-10-04 23:43:52 -0300

    Improved Stocks list

--- data/stocks.py
+++ data/stocks.py
@@ -1,10 +1,13 @@
+'''
+Dictonary with stocks and its starting values
+'''
 class Stocks():
     def __init__(self):
         self._stock_list = {'CSNA3': 9.06, 'RADL3': 61.70, 'BVMF3': 17.50, 'MULT3': 60.22,
             'SBSP3': 29.09, 'CPFE3': 22.10, 'BBDC4': 25.70, 'BRML3': 12.62, 'BBDC3': 26.85,
             'EQTL3': 49.07, 'CSAN3': 34.29, 'PETR4': 9.50, 'SMLE3': 46.42, 'LREN3': 24.23,
             'KROT3': 13.57, 'PETR3': 11.69, 'USIM5': 2.07, 'CMIG4': 7.56, 'QUAL3': 18.00, 
-            'MRVE3': 11.39}
+            'MRVE3': 11.39, 'SBUX': 85.92, 'FB': 180.45}
 
     def get_stocks(self):
         return self._stock_list
